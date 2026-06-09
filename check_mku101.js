a8a37975a7f7af8219113c4dda38e461b446037c
Suwanto Sanjaya <suwantosanjaya@gmail.com>
2026-06-09 10:27:53 +0700

    edit analitik capaian

--- check_mku101.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
-async function main() {
-  const mku101 = await prisma.subject.findUnique({ where: { code: 'MKU101' }, include: { subjectClos: { include: { clo: true } } } })
-  mku101.subjectClos.forEach(sc => console.log(`${sc.clo.code}: weight ${sc.weight}`))
-}
-
-main().finally(() => prisma.$disconnect())
--- scratch.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const subjects = await prisma.subject.findMany({
-    select: { id: true, code: true, title: true, departmentId: true, facultyId: true, scope: true }
-  })
-  console.log(JSON.stringify(subjects, null, 2))
-}
-main()
